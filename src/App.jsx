997706755987ae50d0e48c74a7e7ff9b4af7584a
Giovani88 <l20120091@morelia.tecnm.mx>
2023-10-16 08:44:34 -0600

    Add Topbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ import {
   Link
 } from "react-router-dom";
 
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes } from 'react-router-dom';
+import Topbar from './scenes/global/Topbar'
+import Dashboard from './scenes/dashboard'
+import Sidebar from './scenes/global/Sidebar'
+
 import Login from "./login";
 import Index from "./index";
 import Registro from "./registro";
@@ -34,10 +41,23 @@ const router = createBrowserRouter([
 ])
 
 function App(){
+  const [theme, colorMode] = useMode()
   return (
-    <div className='App'>
-      <RouterProvider router={router}/>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='App'>
+        {/* <RouterProvider router={router}/> */}
+          <main className='content'>
+            <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard/>} />
+              <Route path='/login' element={<Login/>} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
